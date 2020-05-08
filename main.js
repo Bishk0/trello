52ff9cc7750007534783b6bb82261a4e1b13d31c
Bishko <yurik322412@gmail.com>
2020-05-08 16:00:31 +0300

    adding drag-and-drop functionality

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let noteIdCounter = 8;
 let columnIdCounter = 4;
+let draggedNote = null;
 
 document.querySelectorAll(".column").forEach(columnProcess);
 
 document
   .querySelector("[data-action-addColumn]")
-  .addEventListener("click", function (event) { // create new columns
+  .addEventListener("click", function (event) {
+    // create new columns
     const columnElement = document.createElement("div");
     columnElement.classList.add("column");
     columnElement.setAttribute("draggable", true);
@@ -22,7 +24,8 @@ document
 
 document.querySelectorAll(".note").forEach(noteProcess);
 
-function columnProcess(columnElement) { // create new cards
+function columnProcess(columnElement) {
+  // create new cards
   const spanAction_addNote = columnElement.querySelector(
     "[data-action-addNote]"
   );
@@ -37,18 +40,27 @@ function columnProcess(columnElement) { // create new cards
   });
 
   const headerElement = columnElement.querySelector(".column-header");
-headerElement.addEventListener("dblclick", function (event) {
+  headerElement.addEventListener("dblclick", function (event) {
     headerElement.setAttribute("contenteditable", true);
     headerElement.focus();
-});
+  });
 
-headerElement.addEventListener("blur", function (event) {
+  headerElement.addEventListener("blur", function (event) {
     headerElement.removeAttribute("contenteditable");
-});
+  });
+columnElement.addEventListener("dragover", function (event) {
+    event.preventDefault();
+})
+  columnElement.addEventListener("drop", function (event) {
+    if (draggedNote) {
+        return columnElement.querySelector("[data-notes]").append(draggedNote);
+    }
+  });
 
 }
 
-function noteProcess(noteElement) { // allow edit card
+function noteProcess(noteElement) {
+  // allow edit card
   noteElement.addEventListener("dblclick", function (event) {
     noteElement.setAttribute("contenteditable", true);
     noteElement.focus();
@@ -56,4 +68,70 @@ function noteProcess(noteElement) { // allow edit card
   noteElement.addEventListener("blur", function (event) {
     noteElement.removeAttribute("contenteditable");
   });
+
+  //------drag-and-drop-----
+  noteElement.addEventListener("dragstart", dragstart_noteHandler);
+  noteElement.addEventListener("dragend", dragend_noteHandler);
+  noteElement.addEventListener("dragenter", dragenter_noteHandler);
+  noteElement.addEventListener("dragover", dragover_noteHandler);
+  noteElement.addEventListener("dragleave", dragleave_noteHandler);
+  noteElement.addEventListener("drop", drop_noteHandler);
+}
+
+function dragstart_noteHandler(event) {
+  draggedNote = this;
+  this.classList.add("dragged");
+  //   console.log("dragstart", event, this);
+  event.stopPropagation();
+}
+function dragend_noteHandler(event) {
+  draggedNote = null;
+  this.classList.remove("dragged");
+
+  document
+    .querySelectorAll(".note")
+    .forEach((x) => x.classList.remove("under"));
+  //   console.log("dragend", event, this);
+}
+function dragenter_noteHandler(event) {
+  if (this === draggedNote) {
+    return;
+  }
+  this.classList.add("under");
+  //   console.log("dragenter", event, this);
+}
+function dragover_noteHandler(event) {
+  event.preventDefault();
+
+  if (this === draggedNote) {
+    return;
+  }
+  //   console.log("dragover", event, this);
+}
+function dragleave_noteHandler(event) {
+  if (this === draggedNote) {
+    return;
+  }
+  this.classList.remove("under");
+  //   console.log("dragleave", event, this);
+}
+function drop_noteHandler(event) {
+    event.stopPropagation();
+  if (this === draggedNote) {
+    return;
+  }
+  //   console.log("drop", event, this);
+  if (this.parentElement === draggedNote.parentElement) {
+    const note = Array.from(this.parentElement.querySelectorAll(".note"));
+    const indexA = note.indexOf(this);
+    const indexB = note.indexOf(draggedNote);
+    if (indexA < indexB) {
+        this.parentElement.insertBefore(draggedNote, this);
+    } else {
+        this.parentElement.insertBefore(draggedNote, this.nextElementSibling);
+    }
+  } else {
+    this.parentElement.insertBefore(draggedNote, this);
+  }
+
 }
