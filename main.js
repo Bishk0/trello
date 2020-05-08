3b5525d05012bced185d3e10341f57cfdce55a73
Bishko <yurik322412@gmail.com>
2020-05-08 13:56:03 +0300

    allow to edit cards

--- main.js
+++ main.js
@@ -5,26 +5,24 @@ document.querySelectorAll(".column").forEach(columnProcess);
 
 document
   .querySelector("[data-action-addColumn]")
-  .addEventListener("click", function (event) {
+  .addEventListener("click", function (event) { // create new columns
     const columnElement = document.createElement("div");
     columnElement.classList.add("column");
     columnElement.setAttribute("draggable", "true");
     columnElement.setAttribute("data-note-id", columnIdCounter);
-
     columnElement.innerHTML = `
-     <p class="column-header" contenteditable="true">New column</p>
+     <p class="column-header">New column</p>
        <div data-notes></div>
      <p class="column-footer"> <span data-action-addNote class="action">+ Добавити картку</span></p>
     `;
-
     columnIdCounter++;
-
     document.querySelector(".columns").append(columnElement);
-
     columnProcess(columnElement);
   });
 
-function columnProcess(columnElement) {
+document.querySelectorAll(".note").forEach(noteProcess);
+
+function columnProcess(columnElement) { // create new cards
   const spanAction_addNote = columnElement.querySelector(
     "[data-action-addNote]"
   );
@@ -35,5 +33,16 @@ function columnProcess(columnElement) {
     noteElement.setAttribute("data-note-id", noteIdCounter);
     noteIdCounter++;
     columnElement.querySelector("[data-notes]").append(noteElement);
+    noteProcess(noteElement);
+  });
+}
+
+function noteProcess(noteElement) { // allow edit card
+  noteElement.addEventListener("dblclick", function (event) {
+    noteElement.setAttribute("contenteditable", "true");
+    noteElement.focus();
+  });
+  noteElement.addEventListener("blur", function (event) {
+    noteElement.removeAttribute("contenteditable");
   });
 }
